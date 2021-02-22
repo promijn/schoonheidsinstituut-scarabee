acfadec44d8fea6968ad728edaf46a8358985d19
Paul Romijn <paul@bluecloudcompany.com>
2021-02-22 11:04:44 +0100

    first setup styling changes

--- assets/js/proposure.js
+++ assets/js/proposure.js
@@ -117,3 +117,49 @@ jQuery(document).ready(function ($) {
 
 
 });
+
+(function($){
+    function equalizeHeights(selector) {
+        var heights = new Array();
+
+        // Loop to get all element heights
+        $(selector).each(function() {
+
+            // Need to let sizes be whatever they want so no overflow on resize
+            $(this).css('min-height', '0');
+            $(this).css('max-height', 'none');
+            $(this).css('height', 'auto');
+
+            // Then add size (no units) to array
+            heights.push($(this).height());
+        });
+
+        // Find max height of all elements
+        var max = Math.max.apply( Math, heights );
+
+        // Set all heights to max height
+        $(selector).each(function() {
+            $(this).css('height', max + 'px');
+        });
+    }
+
+    $(window).load(function() {
+        // Fix heights on page load
+        //equalizeHeights('#bootstack-pricing .panel-body');
+        equalizeHeights('#bootstack-pricing .panel-body');
+        equalizeHeights('#bootstack-pricing .row-2 .panel-body');
+        equalizeHeights("#content-section-1 .thumbnail");
+
+        // Fix heights on window resize
+        $(window).resize(function() {
+
+            // Needs to be a timeout function so it doesn't fire every ms of resize
+            setTimeout(function() {
+                equalizeHeights('#bootstack-pricing .panel-body');
+                equalizeHeights('#bootstack-pricing .row-2 .panel-body');
+                equalizeHeights('#bootstack-pricing .thumbnail');
+            }, 120);
+        });
+    });
+})(jQuery);
+
